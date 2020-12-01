9ed806d73b9f0cfad04191b5708ee50bee43f60a
waghrahul60 <waghrahul60@gmail.com>
2020-12-01 08:28:19 +0530

    Add project files.

---
+++ Lecture/Day1/BasicClassConcepts/Demo.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicClassConcepts
+{
+    class Demo
+    {
+        static void Main(string[] args)
+        {
+            /*
+            Console.WriteLine("sizeof(int): {0}", sizeof(int));//4
+            Console.WriteLine("sizeof(float): {0}", sizeof(float));//4
+            Console.WriteLine("sizeof(char): {0}", sizeof(char));//2
+            Console.WriteLine("sizeof(double): {0}", sizeof(double));//8
+            Console.WriteLine("sizeof(bool): {0}", sizeof(bool));//1-bit
+
+            Console.WriteLine("sizeof(byte)     : {0}", sizeof(byte));
+            Console.WriteLine("sizeof(sbyte)    : {0}", sizeof(sbyte));
+            Console.WriteLine("sizeof(short)    : {0}", sizeof(short));
+            Console.WriteLine("sizeof(ushort)   : {0}", sizeof(ushort));
+            Console.WriteLine("sizeof(char)     : {0}", sizeof(char));
+            Console.WriteLine("sizeof(int)      : {0}", sizeof(int));
+            Console.WriteLine("sizeof(float)    : {0}", sizeof(float));
+            Console.WriteLine("sizeof(double)   : {0}", sizeof(double));
+            Console.WriteLine("sizeof(bool)     : " + sizeof(bool));
+
+            
+
+            int a = 10;
+            int b = 3;
+
+            int sum = a + b;
+            int sub = a - b;
+            int mul = a + b;
+            float div = (float)(a/b);
+            Console.WriteLine("Addition of {0} and {1} is = {2}",a,b,sum);
+            Console.WriteLine($"Addition of {a} and {b} is = {sum}");
+            */
+            int a;
+            int b;
+            int min;
+            int max;
+
+            //input numbers
+            Console.Write("Enter first number : ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter second number: ");
+            b = Convert.ToInt32(Console.ReadLine());
+
+
+            //finding max number using if-else
+            if (a > b)
+                max = a;
+            else
+                max = b;
+
+            //finding min number using if else
+            if (a < b)
+                min = a;
+            else
+                min = b;
+
+            //printing
+            Console.WriteLine("Using if-else...");
+            Console.WriteLine("Minimum number = {0}", min);
+            Console.WriteLine("Maximum number = {0}", max);
+
+            //finding max number using ternary operator
+            max = (a > b) ? a : b;
+
+            //finding min number using ternary operator
+            min = (a < b) ? a : b;
+
+            //printing
+            Console.WriteLine("Using Ternary operator...");
+            Console.WriteLine("Minimum number = {0}", min);
+            Console.WriteLine("Maximum number = {0}", max);
+
+            //hit ENTER to exit the program
+            Console.ReadLine();
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lecture/Day1/BasicClassConcepts/Program.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace BasicClassConcepts
+{
+    class Program
+    {
+        static void oldMain()
+        {
+            //class2
+            //BasicClassConcepts.class2
+            //n2.class2
+            //System.Data.DataSet
+            // n2.n3.class4;
+            //System.Console.WriteLine("Hello World");
+            Console.WriteLine("aa");
+            System.Console.ReadLine();
+
+            System.Data.DataSet ds = new System.Data.DataSet();
+            DataSet qwe = new DataSet();
+        }
+
+        static void Main()
+        {
+            //Class1.Display()  You can not colled that
+            class1 o; // o is a reference of type Class1
+            o = new class1(); // Allocate memory for an object of type Class1 and make o point to it
+
+            //class1 o = new class1();
+            o.Display();
+            o.Display("Rahul");
+            
+            //Positional parameters
+            Console.WriteLine(o.Add(10, 20, 30, 40));
+            Console.WriteLine(o.Add(10, 20, 30));
+            Console.WriteLine(o.Add(10, 20));
+            Console.WriteLine(o.Add(10));
+            Console.WriteLine(o.Add());
+
+            // Named Parametsre
+            Console.WriteLine(o.Add(d: 40));
+            Console.WriteLine(o.Add(c: 30));
+            Console.WriteLine(o.Add(d: 40, c: 30));
+
+
+            //Add2
+            //Console.WriteLine(o.Add2(d: 40));
+            Console.WriteLine(o.Add2(d: 40, a: 40, b: 40, c: 40));
+
+
+
+            Console.ReadLine();
+
+        }
+    }
+    public class class1
+    {
+        public void Display()
+        {
+            System.Console.WriteLine("Display");
+        }
+
+        //function Overloading
+        public void Display(String s)
+        {
+            //Console.WriteLine("\\n");
+            System.Console.WriteLine("Display \n " + s);
+        }
+
+        //Optional parameters with Default values
+        //If You are giving Optional parametr All Parameters to It's right all are optional (Positional parameters)
+        public int Add(int a= 0, int b= 0, int c= 0, int d= 0) //Default parameters (another way of Overloading)
+        {
+            return a + b + c + d;
+        }
+        //public int Add(int a, int b, int c)
+        //{
+        //    return a + b + c;
+        //}
+        //public int Add(int a, int b)
+        //{
+        //    return a + b;
+        //}
+
+        public int Add2(int a , int b , int c , int d ) 
+        {
+            return a + b + c + d;
+        }
+    }
+}
+
+namespace n2
+{
+    class C3
+    {
+
+    }
+    class class2
+    {
+
+    }
+    namespace n3
+    {
+        class class4
+        {
+
+        }
+    }
+}
